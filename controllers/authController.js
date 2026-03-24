6e41c8e73cd1f71b68d39e067bc35e15a840ed80
Your Name <nirajkumar09160@gmail.com>
2026-03-24 15:48:41 +0530

    auth controller done

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const generateToken = require("../utils/generateToken")
+
+// Register 
+
+exports.register = async(req,res)=>{
+      const {email, password} = req.body
+
+      const hashed = await bcrypt.hash(password, 10)
+
+      const user = await User.create({
+            email, password:hashed
+      })
+
+      res.json({ token : generateToken(user._id)})
+}
+
+// Login
+
+exports.login = async (req,res)=>{
+      const {email, password} = rea.body;
+      const user = await User.findOne({email})
+
+      if (!user) return res.status(400).json({msg :"User not found"})
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (!isMatch) return res.status(400).json({msg: "Wrong password !"})
+
+      res.json({token : generateToken(user._id)})
+}
